774758f32ac0b65728b4f5a7132e50ded06ce788
qiaojun.xiao <qiaojun.xiao@zupeng.com>
2022-04-12 11:22:24 +0800

    first commit

---
+++ src/test/java/com/aicoding/swapper/SwapperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aicoding.swapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwapperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
